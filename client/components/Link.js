5f7a2e68766e915cbd191fa1b9bb7d6a9fa5d2d1
David Bermudez <davidpiber@hotmail.com>
2017-09-24 21:23:17 -0600

    link changes

--- client/components/Link.js
+++ client/components/Link.js
@@ -5,11 +5,9 @@ const Link = ({ active, children, onClick }) => {
     if (active) {
         return <span>{children}</span>
     }
-
     return (
         <a href='#' onClick={(e)=>{e.preventDefault; onClick()}}>{children}</a>
     )
-
 }
 
 Todo.PropTypes = {
